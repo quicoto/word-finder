16b410bc0212b90d0f265c0191dc4e6758dff766
quicoto <torres.rick@gmail.com>
2021-12-31 19:34:38 +0000

    Deploying to gh-pages from @ quicoto/word-finder@e0508bad2fdf8483099ce2a6f94abfd6587c76c6 🚀

--- js/main.js
+++ js/main.js
@@ -1,1 +1,294 @@
-(()=>{var h=Object.create;var $=Object.defineProperty;var x=Object.getOwnPropertyDescriptor;var y=Object.getOwnPropertyNames;var q=Object.getPrototypeOf,R=Object.prototype.hasOwnProperty;var A=t=>$(t,"__esModule",{value:!0});var D=(t,e)=>()=>(e||t((e={exports:{}}).exports,e),e.exports);var _=(t,e,s,o)=>{if(e&&typeof e=="object"||typeof e=="function")for(let r of y(e))!R.call(t,r)&&(s||r!=="default")&&$(t,r,{get:()=>e[r],enumerable:!(o=x(e,r))||o.enumerable});return t},p=(t,e)=>_(A($(t!=null?h(q(t)):{},"default",!e&&t&&t.__esModule?{get:()=>t.default,enumerable:!0}:{value:t,enumerable:!0})),t);var c=D((W,b)=>{var C={isValid:"bg-success",isSemiValid:"bg-warning",isInvalid:"is-invalid",letterInput:"letter-input",word:"word",wordDisabled:"word--disabled"};b.exports.CLASSES=C});var w=p(c());var d=p(c()),l={letterInput:`.${d.CLASSES.letterInput}`,letterInputInvalid:`.${d.CLASSES.letterInput}.${d.CLASSES.isInvalid}`,rows:"#rows",word:`.${d.CLASSES.word}`,wordDisabled:`.${d.CLASSES.wordDisabled}`};var I=p(c());function k(t){let{name:e,value:s,disabled:o}=t,r=`${I.CLASSES.letterInput} form-control form-control-lg text-center text-uppercase`;return`<input ${o?"disabled":""} class="${r}" type="text" maxlength="1" name="${e}" aria-label="${e}" ${s?`value=${s}`:""} required="required">`}function T(t){let e="";return t.forEach(s=>{let{name:o,value:r,disabled:n}=s;e+=`<div class="letter-wrapper">${k({name:o,value:r,disabled:n})}</div>`}),e}function v(t){let{initialValue:e}=t,s="";for(let o=0;o<6;o+=1){let r=[],n=!0;e[o]?.disabled===!1&&(n=!1);for(let i=0;i<5;i+=1){let m="";e[o]?.letters[i]&&(m=e[o].letters[i].letter),r.push({name:`${o}-${i}`,value:m,disabled:n})}s+=`<div data-row="${o}" class="word d-flex justify-content-center ${n?"word--disabled":""}">${T(r)}</div>`}return s}var f=p(c());function E(t){let e=document.querySelector(`${l.rows} ${l.word}[data-row="${t}"]`);e.classList.remove(f.CLASSES.wordDisabled),e.querySelectorAll(l.letterInput).forEach((o,r)=>{o.disabled=!1,r===0&&o.focus()})}function S(t){t.querySelectorAll(l.letterInput).forEach(s=>{s.disabled=!0}),t.classList.add(f.CLASSES.wordDisabled)}function L(t,e){let s=[];return e.split("").forEach((o,r)=>{let n=-1;t.indexOf(o)>-1&&(t[r]===o?n=2:n=1),s.push({letter:o,status:n})}),s}function g(t){t.forEach((e,s)=>{e.forEach((o,r)=>{let{status:n}=o,i=document.querySelector(`${l.letterInput}[name="${s}-${r}"]`);n===1&&i.classList.add(f.CLASSES.isSemiValid),n===2&&i.classList.add(f.CLASSES.isValid)})})}var u={},a={database:[],language:"english",data:[{letters:[{letter:"",status:0},{letter:"",status:0},{letter:"",status:0},{letter:"",status:0},{letter:"",status:0}],disabled:!1}]};function j(){u.rows=document.getElementById("rows"),u.form=document.querySelector("form"),u.submitButton=document.getElementById("submit")}function O(){let t=document.getElementById("fireworks_wrapper");t.querySelector(".fireworks").classList.add("pyro"),setTimeout(()=>{t.querySelector(".fireworks").classList.remove("pyro")},1e4)}function M(t){let e=t.findIndex(s=>s.status<2)===-1;if(e){let s=document.querySelector(`${l.rows} ${l.word}:not(${l.wordDisabled})`);u.submitButton.disabled=!0,S(s),O()}return e}function N(t){t.preventDefault();let e="",s=document.querySelector(`${l.rows} ${l.word}:not(${l.wordDisabled})`);if(s.querySelectorAll(l.letterInputInvalid).length>0)return;s.querySelectorAll(l.letterInput).forEach(i=>{e+=i.value}),S(s),E(+s.dataset.row+1);let n=L(a.solution,e);a.data[+s.dataset.row]=n,g(a.data),M(n)}function z(t){let{target:e}=t;if(e.classList.contains(w.CLASSES.letterInput)){let{value:s,name:o}=e,r=/[a-zA-Z]/g,n=s.match(r),i=+o.split("-")[1];if(i<4){let m=`${o.split("-")[0]}-${i+1}`;document.querySelector(`${l.letterInput}[name="${m}"]`).focus()}else i===4&&u.submitButton.focus();n?e.classList.remove(w.CLASSES.isInvalid):e.classList.add(w.CLASSES.isInvalid)}}function H(){u.form.addEventListener("submit",N),document.addEventListener("keyup",z)}function K(){j(),H(),fetch(`./database/${a.language}.json?${new Date().getTime()}`).then(t=>t.json()).then(t=>{let e=Math.floor(Math.random()*(t.words.length+1));a.database=t.words,a.solution=t.words[e].split(""),console.log(a.solution),u.rows.innerHTML=v({initialValue:a.data})})}K();})();
+(() => {
+  var __create = Object.create;
+  var __defProp = Object.defineProperty;
+  var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+  var __getOwnPropNames = Object.getOwnPropertyNames;
+  var __getProtoOf = Object.getPrototypeOf;
+  var __hasOwnProp = Object.prototype.hasOwnProperty;
+  var __markAsModule = (target) => __defProp(target, "__esModule", { value: true });
+  var __commonJS = (cb, mod) => function __require() {
+    return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
+  };
+  var __reExport = (target, module, copyDefault, desc) => {
+    if (module && typeof module === "object" || typeof module === "function") {
+      for (let key of __getOwnPropNames(module))
+        if (!__hasOwnProp.call(target, key) && (copyDefault || key !== "default"))
+          __defProp(target, key, { get: () => module[key], enumerable: !(desc = __getOwnPropDesc(module, key)) || desc.enumerable });
+    }
+    return target;
+  };
+  var __toESM = (module, isNodeMode) => {
+    return __reExport(__markAsModule(__defProp(module != null ? __create(__getProtoOf(module)) : {}, "default", !isNodeMode && module && module.__esModule ? { get: () => module.default, enumerable: true } : { value: module, enumerable: true })), module);
+  };
+
+  // src/js/CLASSES.js
+  var require_CLASSES = __commonJS({
+    "src/js/CLASSES.js"(exports, module) {
+      var CLASSES5 = {
+        isValid: "bg-success",
+        isSemiValid: "bg-warning",
+        isInvalid: "is-invalid",
+        letterInput: "letter-input",
+        word: "word",
+        wordDisabled: "word--disabled"
+      };
+      module.exports.CLASSES = CLASSES5;
+    }
+  });
+
+  // src/js/main.js
+  var import_CLASSES4 = __toESM(require_CLASSES());
+
+  // src/js/SELECTORS.js
+  var import_CLASSES = __toESM(require_CLASSES());
+  var SELECTORS = {
+    letterInput: `.${import_CLASSES.CLASSES.letterInput}`,
+    letterInputInvalid: `.${import_CLASSES.CLASSES.letterInput}.${import_CLASSES.CLASSES.isInvalid}`,
+    rows: "#rows",
+    word: `.${import_CLASSES.CLASSES.word}`,
+    wordDisabled: `.${import_CLASSES.CLASSES.wordDisabled}`
+  };
+
+  // src/js/templates.js
+  var import_CLASSES2 = __toESM(require_CLASSES());
+  function input(config) {
+    const {
+      name,
+      value,
+      disabled
+    } = config;
+    const classes = `${import_CLASSES2.CLASSES.letterInput} form-control form-control-lg text-center text-uppercase`;
+    return `<input ${disabled ? "disabled" : ""} class="${classes}" type="text" maxlength="1" name="${name}" aria-label="${name}" ${value ? `value=${value}` : ""} required="required">`;
+  }
+  function row(columns) {
+    let html = "";
+    columns.forEach((column) => {
+      const {
+        name,
+        value,
+        disabled
+      } = column;
+      html += `<div class="letter-wrapper">${input({
+        name,
+        value,
+        disabled
+      })}</div>`;
+    });
+    return html;
+  }
+  function game(config) {
+    const { initialValue } = config;
+    let html = "";
+    for (let i = 0; i < 6; i += 1) {
+      const columns = [];
+      let disabled = true;
+      if (initialValue[i]?.disabled === false)
+        disabled = false;
+      for (let j = 0; j < 5; j += 1) {
+        let value = "";
+        if (initialValue[i]?.letters[j]) {
+          value = initialValue[i].letters[j].letter;
+        }
+        columns.push({
+          name: `${i}-${j}`,
+          value,
+          disabled
+        });
+      }
+      html += `<div data-row="${i}" class="word d-flex justify-content-center ${disabled ? "word--disabled" : ""}">${row(columns)}</div>`;
+    }
+    return html;
+  }
+
+  // src/js/utils.js
+  var import_CLASSES3 = __toESM(require_CLASSES());
+  function enableNextRow(row2) {
+    const $row = document.querySelector(`${SELECTORS.rows} ${SELECTORS.word}[data-row="${row2}"]`);
+    $row.classList.remove(import_CLASSES3.CLASSES.wordDisabled);
+    const $inputs = $row.querySelectorAll(SELECTORS.letterInput);
+    $inputs.forEach(($input, index) => {
+      $input.disabled = false;
+      if (index === 0)
+        $input.focus();
+    });
+  }
+  function disableRow($row) {
+    const $inputs = $row.querySelectorAll(SELECTORS.letterInput);
+    $inputs.forEach(($input) => {
+      $input.disabled = true;
+    });
+    $row.classList.add(import_CLASSES3.CLASSES.wordDisabled);
+  }
+  function calculate(solution, input2) {
+    const result = [];
+    input2.split("").forEach((letter, index) => {
+      let status = -1;
+      if (solution.indexOf(letter) > -1) {
+        if (solution[index] === letter) {
+          status = 2;
+        } else {
+          status = 1;
+        }
+      }
+      result.push({
+        letter,
+        status
+      });
+    });
+    return result;
+  }
+  function processResults(data) {
+    data.forEach((row2, i) => {
+      row2.forEach((input2, j) => {
+        const { status } = input2;
+        const $input = document.querySelector(`${SELECTORS.letterInput}[name="${i}-${j}"]`);
+        if (status === 1) {
+          $input.classList.add(import_CLASSES3.CLASSES.isSemiValid);
+        }
+        if (status === 2) {
+          $input.classList.add(import_CLASSES3.CLASSES.isValid);
+        }
+      });
+    });
+  }
+
+  // src/js/main.js
+  var _$ = {};
+  var initialData = [
+    {
+      letters: [
+        { letter: "", status: 0 },
+        { letter: "", status: 0 },
+        { letter: "", status: 0 },
+        { letter: "", status: 0 },
+        { letter: "", status: 0 }
+      ],
+      disabled: false
+    }
+  ];
+  function _getLanguage() {
+    let language = "english";
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    const params = Object.fromEntries(urlSearchParams.entries());
+    if (params.lang === "ca")
+      language = "catala";
+    return language;
+  }
+  var _ = {
+    database: [],
+    language: _getLanguage(),
+    data: initialData
+  };
+  function _setElements() {
+    _$.rows = document.getElementById("rows");
+    _$.form = document.querySelector("form");
+    _$.submitButton = document.getElementById("submit");
+    _$.reloadButton = document.getElementById("reload");
+    _$.solution = document.getElementById("solution");
+  }
+  function _fireworks() {
+    const fireworksWrapper = document.getElementById("fireworks_wrapper");
+    fireworksWrapper.querySelector(".fireworks").classList.add("pyro");
+    setTimeout(() => {
+      fireworksWrapper.querySelector(".fireworks").classList.remove("pyro");
+    }, 1e4);
+  }
+  function _checkIfRoundIsSolved(results) {
+    const isDone = results.findIndex((i) => i.status < 2) === -1;
+    if (isDone) {
+      const $currentRow = document.querySelector(`${SELECTORS.rows} ${SELECTORS.word}:not(${SELECTORS.wordDisabled})`);
+      _$.submitButton.hidden = true;
+      _$.reloadButton.hidden = false;
+      disableRow($currentRow);
+      _fireworks();
+    }
+    return isDone;
+  }
+  function _showSolution() {
+    _$.solution.innerText = _.solution.join("");
+    _$.solution.hidden = false;
+    _$.submitButton.hidden = true;
+    _$.reloadButton.hidden = false;
+  }
+  function _onSubmit(DOMEvent) {
+    DOMEvent.preventDefault();
+    let wordToValidate = "";
+    const $currentRow = document.querySelector(`${SELECTORS.rows} ${SELECTORS.word}:not(${SELECTORS.wordDisabled})`);
+    const $invalidInputs = $currentRow.querySelectorAll(SELECTORS.letterInputInvalid);
+    if ($invalidInputs.length > 0)
+      return;
+    const $inputs = $currentRow.querySelectorAll(SELECTORS.letterInput);
+    $inputs.forEach(($input) => {
+      wordToValidate += $input.value;
+    });
+    disableRow($currentRow);
+    const nextRow = +$currentRow.dataset.row + 1;
+    if (nextRow < 6) {
+      enableNextRow(nextRow);
+    }
+    const results = calculate(_.solution, wordToValidate);
+    _.data[+$currentRow.dataset.row] = results;
+    processResults(_.data);
+    const isSolved = _checkIfRoundIsSolved(results);
+    if (!isSolved && nextRow === 6) {
+      _showSolution();
+    }
+  }
+  function _onKeyUp(DOMEvent) {
+    const { target } = DOMEvent;
+    if (target.classList.contains(import_CLASSES4.CLASSES.letterInput)) {
+      const { value, name } = target;
+      const regex = /[a-zA-Z]/g;
+      const found = value.match(regex);
+      const letterPosition = +name.split("-")[1];
+      if (letterPosition < 4) {
+        const nextName = `${name.split("-")[0]}-${letterPosition + 1}`;
+        const $nextLetterInput = document.querySelector(`${SELECTORS.letterInput}[name="${nextName}"]`);
+        $nextLetterInput.focus();
+      } else if (letterPosition === 4) {
+        _$.submitButton.focus();
+      }
+      if (found) {
+        target.classList.remove(import_CLASSES4.CLASSES.isInvalid);
+      } else {
+        target.classList.add(import_CLASSES4.CLASSES.isInvalid);
+      }
+    }
+  }
+  function _onReload() {
+    window.location.reload();
+  }
+  function _randomWord() {
+    const randomNumber = Math.floor(Math.random() * (_.database.length + 1));
+    const word = _.database[randomNumber].split("");
+    console.log(word);
+    return word;
+  }
+  function _addEventListeners() {
+    _$.form.addEventListener("submit", _onSubmit);
+    document.addEventListener("keyup", _onKeyUp);
+    _$.reloadButton.addEventListener("click", _onReload);
+  }
+  function _freshStart() {
+    _addEventListeners();
+    _.solution = _randomWord();
+    _.data = initialData;
+    _$.rows.innerHTML = game({
+      initialValue: _.data
+    });
+  }
+  function _setLanguageButtons() {
+    const $selectedLangaugeButton = document.getElementById(`button-${_.language}`);
+    $selectedLangaugeButton.classList.add("btn-outline-success");
+    $selectedLangaugeButton.classList.remove("btn-outline-secondary");
+  }
+  function _init() {
+    _setElements();
+    _setLanguageButtons();
+    fetch(`./database/${_.language}.json?${new Date().getTime()}`).then((response) => response.json()).then((data) => {
+      _.database = data.words;
+      _freshStart();
+    });
+  }
+  _init();
+})();
