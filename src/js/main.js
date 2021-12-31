e0508bad2fdf8483099ce2a6f94abfd6587c76c6
Ricard Torres <torres.rick@gmail.com>
2021-12-31 20:34:05 +0100

    Add català

--- src/js/main.js
+++ src/js/main.js
@@ -4,27 +4,41 @@ import * as templates from './templates';
 import * as utils from './utils';
 
 const _$ = {};
+const initialData = [
+  {
+    letters: [
+      { letter: '', status: 0 },
+      { letter: '', status: 0 },
+      { letter: '', status: 0 },
+      { letter: '', status: 0 },
+      { letter: '', status: 0 },
+    ],
+    disabled: false,
+  },
+];
+
+function _getLanguage() {
+  let language = 'english';
+  const urlSearchParams = new URLSearchParams(window.location.search);
+  const params = Object.fromEntries(urlSearchParams.entries());
+
+  if (params.lang === 'ca') language = 'catala';
+
+  return language;
+}
+
 const _ = {
   database: [], // Store the language words
-  language: 'english',
-  data: [
-    {
-      letters: [
-        { letter: '', status: 0 },
-        { letter: '', status: 0 },
-        { letter: '', status: 0 },
-        { letter: '', status: 0 },
-        { letter: '', status: 0 },
-      ],
-      disabled: false,
-    },
-  ],
+  language: _getLanguage(),
+  data: initialData,
 };
 
 function _setElements() {
   _$.rows = document.getElementById('rows');
   _$.form = document.querySelector('form');
   _$.submitButton = document.getElementById('submit');
+  _$.reloadButton = document.getElementById('reload');
+  _$.solution = document.getElementById('solution');
 }
 
 function _fireworks() {
@@ -36,18 +50,26 @@ function _fireworks() {
   }, 10000);
 }
 
-function _checkIfRoundIsDone(results) {
+function _checkIfRoundIsSolved(results) {
   const isDone = results.findIndex((i) => i.status < 2) === -1;
 
   if (isDone) {
     const $currentRow = document.querySelector(`${SELECTORS.rows} ${SELECTORS.word}:not(${SELECTORS.wordDisabled})`);
 
-    _$.submitButton.disabled = true;
+    _$.submitButton.hidden = true;
+    _$.reloadButton.hidden = false;
     utils.disableRow($currentRow);
     _fireworks();
   }
 
   return isDone;
+}
+
+function _showSolution() {
+  _$.solution.innerText = _.solution.join('');
+  _$.solution.hidden = false;
+  _$.submitButton.hidden = true;
+  _$.reloadButton.hidden = false;
 }
 
 function _onSubmit(DOMEvent) {
@@ -68,13 +90,22 @@ function _onSubmit(DOMEvent) {
   });
 
   utils.disableRow($currentRow);
-  utils.enableNextRow(+$currentRow.dataset.row + 1);
+
+  const nextRow = +$currentRow.dataset.row + 1;
+
+  if (nextRow < 6) {
+    utils.enableNextRow(nextRow);
+  }
 
   // Store the results
   const results = utils.calculate(_.solution, wordToValidate);
   _.data[+$currentRow.dataset.row] = results;
   utils.processResults(_.data);
-  _checkIfRoundIsDone(results);
+  const isSolved = _checkIfRoundIsSolved(results);
+
+  if (!isSolved && nextRow === 6) {
+    _showSolution();
+  }
 }
 
 function _onKeyUp(DOMEvent) {
@@ -106,26 +137,51 @@ function _onKeyUp(DOMEvent) {
   }
 }
 
+function _onReload() {
+  window.location.reload();
+}
+
+function _randomWord() {
+  const randomNumber = Math.floor(Math.random() * (_.database.length + 1));
+  const word = _.database[randomNumber].split('');
+
+  // eslint-disable-next-line no-console
+  console.log(word);
+
+  return word;
+}
+
 function _addEventListeners() {
   _$.form.addEventListener('submit', _onSubmit);
   document.addEventListener('keyup', _onKeyUp);
+  _$.reloadButton.addEventListener('click', _onReload);
+}
+
+function _freshStart() {
+  _addEventListeners();
+  _.solution = _randomWord();
+  _.data = initialData;
+  _$.rows.innerHTML = templates.game({
+    initialValue: _.data,
+  });
+}
+
+function _setLanguageButtons() {
+  const $selectedLangaugeButton = document.getElementById(`button-${_.language}`);
+
+  $selectedLangaugeButton.classList.add('btn-outline-success');
+  $selectedLangaugeButton.classList.remove('btn-outline-secondary');
 }
 
 function _init() {
   _setElements();
-  _addEventListeners();
+  _setLanguageButtons();
 
   fetch(`./database/${_.language}.json?${new Date().getTime()}`)
     .then((response) => response.json())
     .then((data) => {
-      const random = Math.floor(Math.random() * (data.words.length + 1));
       _.database = data.words;
-      _.solution = data.words[random].split('');
-      // eslint-disable-next-line no-console
-      console.log(_.solution);
-      _$.rows.innerHTML = templates.game({
-        initialValue: _.data,
-      });
+      _freshStart();
     });
 }
 
